62ae1f47d614934df2a835ba1f2bdcb6e20bdcb4
Tapan Rai <raitapan1@gmail.com>
2016-11-08 11:36:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,29 @@
-console.log('Loaded!');
+$('.loader svg')
+    .velocity(
+        {
+            rotateZ:"360"
+        },
+        {
+            duration:5000
+        }
+    );
+
+$(".loader div")
+    .css({display:'none'})
+    .velocity(
+        "fadeIn",
+        {
+           duration:2000,
+            delay:500
+        }
+    );
+
+$(".loader")
+    .velocity(
+        "fadeOut",
+        {
+            delay:7000,
+            duration:1000,
+            display:"none"
+        }
+    );
