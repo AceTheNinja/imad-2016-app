70e7dc4991bb30b9cb221d33c8e3d2a1dafe5d31
Tapan Rai <raitapan1@gmail.com>
2016-11-09 18:30:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,25 +13,25 @@ app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('./main.js', function (req, res) {
+app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('animate.css', function (req, res) {
+app.get('/animate.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'animate.css'));
 });
-app.get('bootstrap.css', function (req, res) {
+app.get('/bootstrap.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
 });
-app.get('modernizr-2.8.3-respond-1.4.2.min.js', function (req, res) {
+app.get('/modernizr-2.8.3-respond-1.4.2.min.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'modernizr-2.8.3-respond-1.4.2.min.js'));
 });
-app.get('bootstrap.min.js', function (req, res) {
+app.get('/bootstrap.min.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.js'));
 });
-app.get('jquery-1.11.2.min.js', function (req, res) {
+app.get('/jquery-1.11.2.min.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jquery-1.11.2.min.js'));
 });
-app.get('typed.js', function (req, res) {
+app.get('/typed.js', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'typed.js'));
 });
 
