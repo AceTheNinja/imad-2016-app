de878e7cbc02f6ff1b957ac3c848db8fa74e4e7c
Tapan Rai <raitapan1@gmail.com>
2016-11-10 00:51:18 +0530

    made the footer

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('./main.scss')
+    return gulp.src('./style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./ui'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./main.scss', ['sass']);
+    gulp.watch('./style.scss', ['sass']);
 });
--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,3 @@
-$('svg')
-    .velocity(
-        {
-            rotateZ:"360"
-        },
-        {
-            duration:5000
-        }
-    );
 
-$(function(){
-    $("#typed").typed({
-        stringsElement: $('#typed-strings'),
-    });
 
-    $("#endfornow").scroll(function(){
-        $(".container-fluid").velocity("slideUp",{
-            duration:1000
-        })
-    })
-});
 
